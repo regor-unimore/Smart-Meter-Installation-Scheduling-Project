c025d1c543962bba70994e7e0a47c4085a08bf8d
Dario Vezzali <dario.vezzali@gmail.com>
2024-11-28 12:16:17 +0100

    Delete testManager.sh

--- testManager.sh
+++
@@ -1,5 +0,0 @@
-# --solutionMethod = branch-and-cut | --mipGap = 0.000001 | --timeLimit = 3600 | --threads = 1
-./test1.sh
-
-# --solutionMethod = grasp | --alpha = {1, 2, 3} | --beta = {0.70, 0.80, 0.90} | --gamma = {0.20, 0.30, 0.40, 0.50} | --maxIter = {50, 100, 200} |--mipGap = 0.000001 | --timeLimit = 5 | --threads = 1 | --seed = 123
-./test2.sh
